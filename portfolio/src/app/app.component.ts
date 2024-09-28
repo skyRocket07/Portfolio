3b05ebd4e4c83259ec64c329f009356d45f28990
skyRocket07 <skyRocket07@github.com>
2024-09-28 22:53:26 +0530

    Navbar base setup

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './shared/components/button/button.component';
+import { NavbarComponent } from "./features/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,ButtonComponent],
+  imports: [CommonModule, RouterOutlet, ButtonComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
